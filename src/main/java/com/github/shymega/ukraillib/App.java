516c912b7c161d04af1ec8ccfb37d0970b1529af
Dom Rodriguez <shymega@shymega.org.uk>
2020-08-25 21:01:51 +0100

    [chore](App.java): Remove unused imports
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- src/main/java/com/github/shymega/ukraillib/App.java
+++ src/main/java/com/github/shymega/ukraillib/App.java
@@ -1,21 +1,7 @@
 package org.shymega.rail;
 
-import com.thalesgroup.rtti._2017_10_01.ldbsv.types.ServiceItem;
-import com.thalesgroup.rtti._2013_11_28.token.types.AccessToken;
-import com.thalesgroup.rtti._2017_10_01.ldbsv.GetBoardByCRSParams;
-import com.thalesgroup.rtti._2017_10_01.ldbsv.GetBoardResponseType;
-import com.thalesgroup.rtti._2017_10_01.ldbsv.LDBSVServiceSoap;
-import com.thalesgroup.rtti._2017_10_01.ldbsv.Ldbsv;
-import com.thalesgroup.rtti._2017_10_01.ldbsv.types.StationBoard;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeFactory;
-import javax.xml.datatype.XMLGregorianCalendar;
-import java.util.GregorianCalendar;
-import java.util.List;
 
 public class App
 {
